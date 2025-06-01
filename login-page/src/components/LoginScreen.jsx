5994b88ffb2fe8deb95687d9d06ccfe4b07c0f1e
Lasith Amanda <lasithamanda1999@example.com>
2025-06-01 20:30:16 +0530

    set default usename password

--- login-page/src/components/LoginScreen.jsx
+++ login-page/src/components/LoginScreen.jsx
@@ -3,8 +3,8 @@ import axios from '../services/axiosInstance';
 import '../components/LoginScreen.css';
 function LoginScreen() {
     const [formData, setFormData] = useState({
-        username: '',
-        password: '',
+        username: 'user',
+        password: 'userpass',
         expiresInMins: 30,
     });
 
