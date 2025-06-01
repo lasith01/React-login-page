40530f42fd49283ea27a2269e6e938ad5c37dd12
Lasith Amanda <lasithamanda1999@example.com>
2025-06-01 19:16:23 +0530

    add navbar and login screen css

--- login-page/src/components/LoginScreen.jsx
+++ login-page/src/components/LoginScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useState} from 'react';
 import axios from '../services/axiosInstance';
+import '../components/LoginScreen.css';
 
 function LoginScreen() {
     const [formData, setFormData] = useState({
